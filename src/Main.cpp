54b9d08d0a3589d352962bd557838bc372f8db1e
GilbertNordhammar <gilbertnordhammar@gmail.com>
2020-07-11 22:30:18 +0200

    Tried out using textures

--- src/Main.cpp
+++ src/Main.cpp
@@ -19,6 +19,9 @@ unsigned int setVertexInput
     float texCoords[],
     unsigned int texCoordsInBytes
 );
+void configureVertexAttributes();
+enum class ImageType {JPG, PNG};
+unsigned int generateAndBindTex2D(const char* texSrc, ImageType type);
 
 int main() {
     glfwInit();
@@ -46,11 +49,11 @@ int main() {
                      fileUtils::getFullPath("shaders/TestShader/TestShader.frag").c_str());
 
     float vertices[] = {
-        // positions         // colors
-         0.5f,  0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  // top right
-         0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  // bottom right
-        -0.5f, -0.5f, 0.0f,  0.0f, 0.0f, 1.0f,  // bottom left
-        -0.5f,  0.5f, 0.0f,  0.0f, 1.0f, 1.0f  // top left 
+        // positions         // colors          // texture coords
+         0.5f,  0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  1.0f, 1.0f,    // top right
+         0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  1.0f, 0.0f,    // bottom right
+        -0.5f, -0.5f, 0.0f,  0.0f, 0.0f, 1.0f,  0.0f, 0.0f,    // bottom left
+        -0.5f,  0.5f, 0.0f,  0.0f, 1.0f, 1.0f,  0.0f, 1.0f     // top left 
     };
     unsigned int indices[] = {
         0, 1, 3,   // first triangle
@@ -63,14 +66,21 @@ int main() {
     };
     auto VAO = setVertexInput(vertices, sizeof(vertices), indices, sizeof(indices), texCoords, sizeof(texCoords));
 
+    stbi_set_flip_vertically_on_load(true);
     executeRenderLoop(window, ourShader, VAO);
 
     glfwTerminate();
     return 0;
 }
 
-
 void executeRenderLoop(GLFWwindow* window, Shader shader, unsigned int VAO) {
+    auto texture = generateAndBindTex2D(fileUtils::getFullPath("resources/textures/wooden-container.jpg").c_str(), ImageType::JPG);
+    auto texture1 = generateAndBindTex2D(fileUtils::getFullPath("resources/textures/awesomeface.png").c_str(), ImageType::PNG);
+
+    shader.use();
+    shader.setInt("texture1", 0);
+    shader.setInt("texture2", 1);
+
     while (!glfwWindowShouldClose(window))
     {
         processInput(window);
@@ -78,6 +88,12 @@ void executeRenderLoop(GLFWwindow* window, Shader shader, unsigned int VAO) {
         clearBuffers();
 
         shader.use();
+
+        glActiveTexture(GL_TEXTURE0); // activate the texture unit first before binding texture
+        glBindTexture(GL_TEXTURE_2D, texture);
+
+        glActiveTexture(GL_TEXTURE1); // activate the texture unit first before binding texture
+        glBindTexture(GL_TEXTURE_2D, texture1);
 
         glBindVertexArray(VAO);
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0); // TODO: makes number of indices not hardcoded
@@ -110,18 +126,56 @@ unsigned int setVertexInput
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, indicesInBytes, indices, GL_STATIC_DRAW);
 
-    // position attribute
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-
-    // color attribute
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
-    glEnableVertexAttribArray(1);
+    configureVertexAttributes();
 
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
 
     return VAO;
+}
+
+unsigned int generateAndBindTex2D(const char* texSrc, ImageType type) {
+    // Load and bind texture
+    unsigned int texture;
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    
+    // set the texture wrapping/filtering options (on the currently bound texture object)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    // load and generate the texture
+    int width, height, nrChannels;
+    unsigned char* data = stbi_load(texSrc, &width, &height, &nrChannels, 0);
+    if (data)
+    {
+        auto pixelFormat = type == ImageType::JPG ? GL_RGB : GL_RGBA;
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, pixelFormat, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cout << "Failed to load texture at " << texSrc << std::endl;
+    }
+    stbi_image_free(data);
+
+    return texture;
+}
+
+void configureVertexAttributes() {
+    // position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    // color attribute
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    // texture attribute
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
 }
 
 GLFWwindow* createWindow(int width, int height) {
--- src/shaders/TestShader/TestShader.frag
+++ src/shaders/TestShader/TestShader.frag
@@ -1,10 +1,14 @@
 #version 330 core
-out vec4 FragColor;
 
 in vec3 ourColor;
-uniform vec3 col;
+in vec2 TexCoord;
+
+uniform sampler2D texture1;
+uniform sampler2D texture2;
+
+out vec4 FragColor;
 
 void main()
 {
-    FragColor = vec4(ourColor, 1.0f);
+    FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.2);
 }
